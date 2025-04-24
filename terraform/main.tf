28d4a430225928a554febb91c356f5a4c08cce0e
Guido Paliot <guido@paliot.de>
2025-04-24 08:51:44 +0200

    Add provider and insecurity :)

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,16 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source = "telmate/proxmox"
+    }
+  }
+}
+
 provider "proxmox" {
   pm_api_url = var.pm_api_url
   pm_user    = var.pm_user
   pm_password = var.pm_password
-  pm_tls_insecure = false
+  pm_tls_insecure = true
 }
 
 variable "pm_api_url" {}
